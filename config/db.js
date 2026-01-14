805d73e7038981a3fcd868b44dee4818682bf913
kaifkhurshid7 <kaifkhurshid18@gmail.com>
2026-01-14 14:26:02 +0530

    minor

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/acmmedia");
+        await mongoose.connect("mongodb+srv://ucse23032_db_user:fjcv11HF42fURQ9O@cluster0.j6asufl.mongodb.net/acmmedia");
         console.log("MongoDB connected");
     } catch (err) {
         console.error("MongoDB connection error");
