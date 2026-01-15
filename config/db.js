12582d759d12732567a0de78ccf948c012e77220
kaifkhurshid7 <kaifkhurshid18@gmail.com>
2026-01-16 01:56:01 +0530

    fixes db

--- config/db.js
+++ config/db.js
@@ -5,10 +5,10 @@ const connectDB = async () => {
         const db = process.env.MONGO_URI || "mongodb+srv://ucse23032_db_user:fjcv11HF42fURQ9O@cluster0.j6asufl.mongodb.net/acmmedia";
         await mongoose.connect(db);
         console.log("------------------------------");
-        console.log("✅ MongoDB Connected Successfully");
+        console.log(" MongoDB Connected Successfully");
         console.log("------------------------------");
     } catch (err) {
-        console.error("❌ MongoDB Connection Error:", err.message);
+        console.error("MongoDB Connection Error:", err.message);
     }
 };
 
